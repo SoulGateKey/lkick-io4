d728924a0b3479a27f1f333b2a572aba802cd376
SoulGateKey <soulgatekey@gmail.com>
2025-02-17 23:00:42 +0800

    update pins define

--- stdinclude.h
+++ stdinclude.h
@@ -34,33 +34,33 @@
 #include "hardware/flash.h"
 #include "config.h"
 
-#define BTN_L1_PIN 0
-#define LED_L1_PIN 1
-#define BTN_L2_PIN 2
-#define LED_L2_PIN 3
-#define BTN_L3_PIN 4
-#define LED_L3_PIN 5
+#define BTN_L1_PIN 26
+#define LED_L1_PIN 22
+#define BTN_L2_PIN 21
+#define LED_L2_PIN 20
+#define BTN_L3_PIN 19
+#define LED_L3_PIN 18
 
-#define BTN_R1_PIN 6
-#define LED_R1_PIN 7
-#define BTN_R2_PIN 8
-#define LED_R2_PIN 9
-#define BTN_R3_PIN 10
-#define LED_R3_PIN 11
+#define BTN_R1_PIN 2
+#define LED_R1_PIN 3
+#define BTN_R2_PIN 4
+#define LED_R2_PIN 5
+#define BTN_R3_PIN 6
+#define LED_R3_PIN 7
 
-#define BTN_LSIDE_PIN 12
-#define LED_LSIDE_PIN 13
-#define BTN_RSIDE_PIN 14
-#define LED_RSIDE_PIN 15
+#define BTN_LSIDE_PIN 17
+#define LED_LSIDE_PIN 16
+#define BTN_RSIDE_PIN 8
+#define LED_RSIDE_PIN 9
 
 // #define BTN_LMENU_PIN 16 // re-wire from 16
 // #define BTN_RMENU_PIN 17 // re-wire from 17
-#define BTN_LMENU_PIN 21 // re-wire from 16
-#define BTN_RMENU_PIN 22 // re-wire from 17
+#define BTN_LMENU_PIN 27 // re-wire from 16
+#define BTN_RMENU_PIN 0 // re-wire from 17
 
-#define BTN_SERV_PIN 18
-#define BTN_TEST_PIN 19
-#define BTN_COIN_PIN 20
+#define BTN_SERV_PIN 12
+#define BTN_TEST_PIN 10
+#define BTN_COIN_PIN 11
 
 #define LEVER_PIN 28
 
